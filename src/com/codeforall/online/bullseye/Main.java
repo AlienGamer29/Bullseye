3e88cb203ffb376b944321334b54a3f0e1f74cea
Kvilarinho <katia.vilas@gmail.com>
2025-08-22 20:19:40 +0100

    Reorganized directories

--- src/com/codeforall/online/bullseye/Main.java
+++ src/com/codeforall/online/bullseye/Main.java
@@ -1,3 +1,5 @@
+package com.codeforall.online.bullseye;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
