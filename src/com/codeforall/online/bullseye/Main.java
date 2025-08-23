84820d980b5093e4d00789f6c6b065df0071c43a
Kvilarinho <katia.vilas@gmail.com>
2025-08-23 20:48:32 +0100

    tests

--- src/com/codeforall/online/bullseye/Main.java
+++ src/com/codeforall/online/bullseye/Main.java
@@ -2,20 +2,42 @@ package com.codeforall.online.bullseye;
 
 import com.codeforall.online.bullseye.game.Arena;
 import com.codeforall.online.bullseye.playables.Target;
+import com.codeforall.online.bullseye.playables.TargetFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+        List<Target> targets = new ArrayList<>();
+        int numberOfTargets = 14;
 
         Arena arena = new Arena();
-        Target target = new Target(900, 300);
+
+        for(int i = 0; i < numberOfTargets; i++) {
+            targets.add(TargetFactory.createTarget());
+        }
+
+
+        //Target target = new Target(900, 300);
+        //Target target1 = new Target(850, 250);
+        //Target target2 = new Target(800, 250);
         System.out.println(arena.getBottom());
         System.out.println(arena.getTop());
 
+
         while (true) {
 
-            Thread.sleep(150);
-            target.update(arena);
+            Thread.sleep(16);
+
+            for (Target t : targets) {
+                t.update(arena);
+            }
+
+            //target.update(arena);
+            //target1.update(arena);
+            //target2.update(arena);
         }
     }
 }
