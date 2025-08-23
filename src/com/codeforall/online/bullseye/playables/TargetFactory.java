654eeebb0fde4c0c6c712115bb2ce7b0a34fe31c
Kvilarinho <katia.vilas@gmail.com>
2025-08-23 20:48:25 +0100

    Created methods that will create targets at random position in a defined area

--- src/com/codeforall/online/bullseye/playables/TargetFactory.java
+++ src/com/codeforall/online/bullseye/playables/TargetFactory.java
@@ -2,7 +2,21 @@ package com.codeforall.online.bullseye.playables;
 
 public class TargetFactory {
 
-    private static int randomPosition() {
-        return 0;
+    // Create a new Target with random X and Y from the predefined values
+    public static Target createTarget() {
+        return new Target(randX(), randY());
     }
+
+    // Create a new Target with random X and Y from the predefined values
+    private static int randX() {
+        int[] values = {900, 850};
+        return values[(int)(Math.random() * values.length)];
+    }
+
+    // Return a random Y position (chosen from fixed values)
+    private static int randY() {
+        int[] values = {100, 200, 300, 400, 500, 600};
+        return values[(int)(Math.random() * values.length)];
+    }
+
 }
