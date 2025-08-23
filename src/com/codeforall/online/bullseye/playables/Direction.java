e2c55f261235e488dd5c633e74293cc83fd112af
Kvilarinho <katia.vilas@gmail.com>
2025-08-23 20:45:07 +0100

    Refactor enum

--- src/com/codeforall/online/bullseye/playables/Direction.java
+++ src/com/codeforall/online/bullseye/playables/Direction.java
@@ -6,24 +6,4 @@ public enum Direction {
     UP,
     DOWN;
 
-
-    public boolean isOpposite(Direction dir) {
-        return dir.equals(oppositeDirection());
-    }
-
-
-    public Direction oppositeDirection() {
-
-        Direction opposite = Direction.UP;
-
-        switch (this) {
-            case UP:
-                opposite = Direction.DOWN;
-                break;
-            case DOWN:
-                opposite = Direction.UP;
-                break;
-        }
-        return opposite;
-    }
 }
