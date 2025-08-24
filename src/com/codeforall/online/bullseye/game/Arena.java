270694497ef12e747e548e8aa05c6d2ddeedc85e
Kvilarinho <katia.vilas@gmail.com>
2025-08-24 14:49:27 +0100

    Some tests on arena - size of picture and canvas, added a few methods

--- src/com/codeforall/online/bullseye/game/Arena.java
+++ src/com/codeforall/online/bullseye/game/Arena.java
@@ -1,6 +1,6 @@
 package com.codeforall.online.bullseye.game;
 
-import com.codeforall.simplegraphics.graphics.Rectangle;
+import com.codeforall.simplegraphics.graphics.Canvas;
 import com.codeforall.simplegraphics.pictures.Picture;
 
 public class Arena {
@@ -8,10 +8,31 @@ public class Arena {
     private int width;
     private int height;
     private Picture picture;
+    private final int BUSHPADDING = 34;
 
     public Arena() {
-        picture = new Picture(-5,0, "resources/background.jpeg");
+
+        picture = new Picture(0, 0, "resources/background.jpeg");
+        picture.grow(-256, -128); // 1534-1024= 512/2 = 256, 1024-768 = 256/2= 128
+        picture.translate(-256, -128); // volta a encostar ao (0,0)
+
+
+        width = picture.getWidth();
+        height = picture.getHeight();
+
+        //Ajusta o canvas
+        Canvas.setMaxX(width - 10);
+        Canvas.setMaxY(height - 10);
+
         picture.draw();
+    }
+
+    public int getTop() {
+        return picture.getY() + BUSHPADDING;
+    }
+
+    public int getBottom() {
+        return picture.getMaxY() - BUSHPADDING;
     }
 
 
