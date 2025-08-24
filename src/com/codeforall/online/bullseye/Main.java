93c5b058b49ce8d5847ef536576aa43bd7069c2d
Kvilarinho <katia.vilas@gmail.com>
2025-08-24 14:37:41 +0100

    tests

--- src/com/codeforall/online/bullseye/Main.java
+++ src/com/codeforall/online/bullseye/Main.java
@@ -10,30 +10,34 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
 
+
         List<Target> targets = new ArrayList<>();
-        int numberOfTargets = 14;
+        int numberOfTargets = 10;
 
         Arena arena = new Arena();
+
 
         for(int i = 0; i < numberOfTargets; i++) {
             targets.add(TargetFactory.createTarget());
         }
 
 
-        //Target target = new Target(900, 300);
+
+        //Target target = new Target(850, 594);
         //Target target1 = new Target(850, 250);
         //Target target2 = new Target(800, 250);
-        System.out.println(arena.getBottom());
-        System.out.println(arena.getTop());
 
 
         while (true) {
 
             Thread.sleep(16);
 
+
             for (Target t : targets) {
                 t.update(arena);
             }
+
+
 
             //target.update(arena);
             //target1.update(arena);
