c10d89c9cdd5b0c2e1e35482dd6cd41ab89ea32e
Carolina-Gau <carolina.gau@gmail.com>
2025-08-24 14:31:54 +0100

    Coisas novas para o teclado

--- src/com/codeforall/online/bullseye/playables/Player.java
+++ src/com/codeforall/online/bullseye/playables/Player.java
@@ -1,8 +1,27 @@
 package com.codeforall.online.bullseye.playables;
 
 public class Player extends Entity {
+    protected int arrowCounter;
 
     public Player(int x, int y) {
         super(x, y);
+    }
+
+    public void moveDOWN() {
+    }
+
+    public void moveUP(){
+    }
+
+    public void shoot(Arrows arrows) {
+        arrowCounter--;
+    }
+
+    public int getArrowCounter(){
+        return arrowCounter;
+    }
+
+    public void setArrowCounter(int counter) {
+        this.arrowCounter = counter;
     }
 }
