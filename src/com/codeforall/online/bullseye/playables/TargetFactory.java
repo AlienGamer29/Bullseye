6773018a81d8a5a4f451492fb47bf6376c46cfc4
Kvilarinho <katia.vilas@gmail.com>
2025-08-24 14:46:52 +0100

    Refactor to distribute targets sequencially in arena when created, instead of random distribuition

--- src/com/codeforall/online/bullseye/playables/TargetFactory.java
+++ src/com/codeforall/online/bullseye/playables/TargetFactory.java
@@ -2,21 +2,35 @@ package com.codeforall.online.bullseye.playables;
 
 public class TargetFactory {
 
-    // Create a new Target with random X and Y from the predefined values
+    private static final int[] X_POS = {850, 780};
+    private static final int[] Y_POS = {34, 174, 314, 454, 594};
+    private static int indexX = 0;
+    private static int indexY = 0;
+    private static final Direction[] DIR = {Direction.DOWN, Direction.UP};
+
+    // Creates a new Target using sequentially distributed X and Y positions.
+    // The initial direction is chosen based on the X position.
     public static Target createTarget() {
-        return new Target(randX(), randY());
+        int x = distributionX();
+        int y = distributionY();
+
+        Direction d = (x == X_POS[0]) ? DIR[0] : DIR[1];
+        return new Target(x, y, d);
     }
 
-    // Create a new Target with random X and Y from the predefined values
-    private static int randX() {
-        int[] values = {900, 850};
-        return values[(int)(Math.random() * values.length)];
+    // Returns the next X position in sequence (alternates between values in X_POS).
+    private static int distributionX() {
+        int x = X_POS[indexX];
+        indexX = (indexX + 1) % X_POS.length;
+        return x;
     }
 
-    // Return a random Y position (chosen from fixed values)
-    private static int randY() {
-        int[] values = {100, 200, 300, 400, 500, 600};
-        return values[(int)(Math.random() * values.length)];
+    // Returns the next Y position in sequence (cycles through values in Y_POS).
+    private static int distributionY() {
+        int y = Y_POS[indexY];
+        indexY = (indexY + 1) % Y_POS.length;
+        return y;
     }
-
 }
+
+
